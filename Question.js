f3f2800fc97a72ccc84fef08eaa3e321db403598
RF-Rifat <rfrifat6344@gmail.com>
2023-04-01 17:44:31 +0600

    first commit

---
+++ Question.js
@@ -0,0 +1,110 @@
+let questions = [  {    numb: 1,    question: "1. What is the value of sin(30°)?",    answer: "0.5",    options: ["1", "0.5", "0.25", "0.75"]
+  },
+  {
+    numb: 2,
+    question: "2. What is the value of √(16) + √(9)?",
+    answer: "7",
+    options: ["5", "7", "10", "12"]
+  },
+  {
+    numb: 3,
+    question: "3. What is the slope of a horizontal line?",
+    answer: "0",
+    options: ["1", "0", "undefined", "infinity"]
+  },
+  {
+    numb: 4,
+    question: "4. What is the area of a circle with a radius of 5 cm?",
+    answer: "78.5 sq. cm",
+    options: ["31.4 sq. cm", "78.5 sq. cm", "157 sq. cm", "314 sq. cm"]
+  },
+  {
+    numb: 5,
+    question: "5. What is the value of log₂(8)?",
+    answer: "3",
+    options: ["2", "3", "4", "5"]
+  },
+  {
+    numb: 6,
+    question: "6. What is the value of tan(45°)?",
+    answer: "1",
+    options: ["0", "1", "2", "π/4"]
+  },
+  {
+    numb: 7,
+    question: "7. What is the equation of a line that passes through the points (2, 3) and (4, 5)?",
+    answer: "y = x + 1",
+    options: ["y = x + 1", "y = 2x + 1", "y = 3x - 1", "y = 2x - 1"]
+  },
+  {
+    numb: 8,
+    question: "8. What is the value of 2³ × 2⁴?",
+    answer: "128",
+    options: ["16", "64", "128", "256"]
+  },
+  {
+    numb: 9,
+    question: "9. What is the value of (a + b)²?",
+    answer: "a² + 2ab + b²",
+    options: ["a² - 2ab + b²", "a² + b²", "a² + 2ab + b²", "a² - b²"]
+  },
+  {
+    numb: 10,
+    question: "10. What is the value of cos(60°)?",
+    answer: "0.5",
+    options: ["1", "0.5", "0.25", "0.75"]
+  },
+  {    numb: 11,    question: "11. The largest 3-digit composite number is ______.",    answer: "999",    options: ["996", "998", "999", "997"]
+  },
+  {
+    numb: 12,
+    question: "12. Which of the following is an irrational number?",
+    answer: "√7",
+    options: ["3/4", "0.25", "π", "√7"]
+  },
+  {
+    numb: 13,
+    question: "13. If a:b = 2:3 and b:c = 4:5, then a:c = ?",
+    answer: "8:15",
+    options: ["6:10", "8:12", "8:15", "10:15"]
+  },
+  {
+    numb: 14,
+    question: "14. If x = 2, y = 3 and z = 4, then the value of (x + y)z is ______.",
+    answer: "20",
+    options: ["9", "14", "20", "24"]
+  },
+  {
+    numb: 15,
+    question: "15. The ratio of the length and breadth of a rectangular field is 5:3. If the perimeter of the field is 64 m, then its length is ______.",
+    answer: "20 m",
+    options: ["18 m", "20 m", "24 m", "30 m"]
+  },
+  {    numb: 16,    question: "16. If x varies inversely as y and x = 8 when y = 3, then find x when y = 6.",    answer: "4",    options: ["2", "3", "4", "6"]
+  },
+  {
+    numb: 17,
+    question: "17. The length, breadth and height of a cuboid are in the ratio 4:3:2. If its volume is 144 cm³, then find its total surface area.",
+    answer: "94 cm²",
+    options: ["78 cm²", "84 cm²", "94 cm²", "108 cm²"]
+  },
+  {
+    numb: 18,
+    question: "18. If the ratio of the areas of two similar triangles is 4:9, then what is the ratio of their corresponding sides?",
+    answer: "2:3",
+    options: ["1:2", "2:3", "3:4", "4:5"]
+  },
+  {
+    numb: 19,
+    question: "19. Find the value of k for which the following system of equations has no solution: 3x + 2y = 7 and kx + 4y = 10.",
+    answer: "6",
+    options: ["3", "4", "5", "6"]
+  },
+  {
+    numb: 20,
+    question: "20. If the sum of the first n terms of an AP is given by Sn = 2n² + 3n, then find its 5th term.",
+    answer: "17",
+    options: ["9", "11", "13", "17"]
+  }
+
+];
